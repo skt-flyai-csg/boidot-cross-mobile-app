4be2f3b94d6ab17cbf2db5788ab13147e178d1db
ChaenLim <limce21@naver.com>
2023-07-28 16:11:00 +0900

    feat: 테마 초기 세팅

--- App.tsx
+++ App.tsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import * as React from 'react';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+declare global {
+  namespace ReactNativePaper {
+    interface ThemeColors {
+      myOwnColor: string;
+    }
+
+    interface Theme {
+      myOwnProperty: boolean;
+    }
+  }
+}
+
+const theme = {
+  ...DefaultTheme,
+  myOwnProperty: true,
+  colors: {
+    ...DefaultTheme.colors,
+    testColor: '#BADA55',
+  },
+};
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <PaperProvider theme={theme}>
+      <SafeAreaView />
+    </PaperProvider>
+  );
 }
---
+++ src/components/StatusBar.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function StatusBar() {
+  return <div>StatusBar</div>;
+}
