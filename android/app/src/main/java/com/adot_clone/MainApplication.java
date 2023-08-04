fc407d2c5c4c78803eb1d0f690ded3361ba5074e
ChaenLim <limce21@naver.com>
2023-08-04 13:43:07 +0900

    add: 음성인식위한 react-native-voice 패키지 추가

--- android/app/src/main/java/com/adot_clone/MainApplication.java
+++ android/app/src/main/java/com/adot_clone/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+import com.wenkesj.voice.VoicePackage;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -21,11 +22,10 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
+          return Arrays.<ReactPackage>asList(
+            new MainReactPackage(),
+            new VoicePackage()
+            );
         }
 
         @Override
