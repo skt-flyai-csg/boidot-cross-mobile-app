edb689da65cfd6e9a8ec528b5f2591c79b58a9bc
ChaenLim <limce21@naver.com>
2023-08-10 11:03:07 +0900

    feat: Bottomsheet footer ui 생성

--- src/screens/ChatBot/index.tsx
+++ src/screens/ChatBot/index.tsx
@@ -1,17 +1,50 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import {useTheme} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Feather';
+import IconMat from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const ChatBot = () => {
   const {colors} = useTheme();
   return (
-    <View
-      style={[
-        {backgroundColor: colors.textNormal, position: 'absolute', bottom: 0},
-      ]}>
-      <Text style={[{color: 'white'}]}>안안녕</Text>
+    <View style={[styles.view]}>
+      <View style={[styles.footer]}>
+        <TouchableOpacity>
+          <Icon name="user" size={24} color={colors.textWhite} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.mic, {backgroundColor: colors.secondary}]}>
+          <Icon name="mic" size={24} color={colors.textWhite} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <IconMat name="keyboard" size={24} color={colors.textWhite} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default ChatBot;
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  mic: {
+    width: 40,
+    height: 40,
+    borderRadius: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  footer: {
+    paddingVertical: 20,
+    marginBottom: 34,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 'auto',
+  },
+});
